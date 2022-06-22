a96eefa77502f699cc6344327d9020c86775ef97
arenasjulian <julian1ar3nas@gmail.com>
2022-06-22 09:29:23 -0500

    pdf

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -10,6 +10,11 @@ router.post("/login", controlador.login);
 router.get("/interfaz", (req, res) => {
   res.render("interfaz");
 });
+
+router.get("/interfaz2", (req, res) => {
+  res.render("interfaz2");
+});
+
 //duvan
 router.get("/prubfact", controlador.prubfact);
 router.get("/valor", controlador.valor);
@@ -31,10 +36,7 @@ router.get('/devolucion',(req, res) => {
 router.get('/ganancias',(req, res) => {
     res.render('ganancias');
 });
-<<<<<<< HEAD
 
-=======
->>>>>>> 32ba16e633ba9db23718e793ab28964a39fd3732
 //RUTA PARA VENTAS-FACTURACION :c
 router.get("/ventas", controlador.ventas); //Esta me trae datos
 router.post("/ventas", controlador.ventas);
@@ -92,10 +94,7 @@ router.get('/devolucion',(req, res) => {
 router.get('/tiempo',(req, res) => {
     res.render('tiempo');
 });
-<<<<<<< HEAD
 
-=======
->>>>>>> 32ba16e633ba9db23718e793ab28964a39fd3732
 //TIEMPO
 
 router.get("/tiempo", controlador.tiempo);
@@ -109,10 +108,7 @@ router.post("/nomina", controlador.consultarnomina);
 //PRODUCTOS
 
 router.post("/productos", controlador.cproductos);
-<<<<<<< HEAD
 
-=======
->>>>>>> 32ba16e633ba9db23718e793ab28964a39fd3732
 
 //BRAYAN
 //TIEMPO CRUD COMPLETO
